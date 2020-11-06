7d6dd08144c9b318e0c2469b5370a94bf9c7817f
terumitomita <70851623+terumitomita@users.noreply.github.com>
2020-11-06 14:28:30 +0900

    1_4課題

--- JavaStudy/1-4/Task1_4.java
+++ JavaStudy/1-4/Task1_4.java
@@ -26,19 +26,19 @@ public class Task1_4 {
          String name = "alice";
          String pass = "pass";
         // ② 名前が「 alice 」、パスワードが「 pass 」であった場合、「 ログイン成功です 」と出力してください。
-        if (CONST_ALICE == name && CONST_PASS == pass) {
+        if (CONST_ALICE.equals(name) && CONST_PASS.equals (pass)){
             System.out.println(CONST_MSG_SUCCESS);
         }
         // ③ 名前のみ正しい場合、「 パスワードに誤りがあります。 」と出力してください。
-        else if (CONST_ALICE == name && CONST_PASS != pass) {
+        else if (CONST_ALICE.equals (name) && !(CONST_PASS.equals (pass))) {
             System.out.println(CONST_MSG_ERROR_NAME);
         }
         // ④ パスワードのみ正しい場合、「 名前に誤りがあります。 」と出力してください。
-        else if (CONST_ALICE != name && CONST_PASS == pass) {
+        else if (!(CONST_ALICE.equals (name)) && CONST_PASS.equals (pass)) {
             System.out.println(CONST_MSG_ERROR_PASS);
         }
         // ⑤ 名前もパスワードも間違っていた場合は、「 入力情報に誤りがあります。 」と出力してください。
-         else if (CONST_ALICE != name && CONST_PASS != pass) {
+        else if (!(CONST_ALICE.equals (name)) && !(CONST_PASS.equals (pass))) {
             System.out.println(CONST_MSG_ERROR_INPUT);
         }
     }
